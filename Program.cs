e53407713435567b78cc8becfc562d08006874f7
pekarasa <33158963+pekarasa@users.noreply.github.com>
2018-02-16 11:55:49 +0100

    Add files via upload
    Generated code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using AtleX;
+using AtleX.CommandLineArguments;
+
+namespace Anteilsscheine
+{
+    class Program
+    {
+        private class MyArgumentsClass : Arguments
+        {
+            [Required]
+            [Display(Description = "This text will be displayed in the help, when requested")]
+            public bool Argument1 { get; set; }
+
+            // Not required
+            [Display(Description = "This text will be displayed in the help, when requested")]
+            public string Name { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            MyArgumentsClass cliArguments;
+            if (!CommandLineArguments.TryParse<MyArgumentsClass>(args, out cliArguments))
+            {
+                // Something wrong, exit or display help?
+                CommandLineArguments.DisplayHelp(cliArguments);
+                return;
+            }
+
+            if (cliArguments.Argument1)
+            {
+
+            }
+        }
+    }
+}
